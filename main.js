a79ff01486c880476dec61a856eb2226f35c126b
ElbekIT <131149159+xakkerelbek@users.noreply.github.com>
2024-09-23 03:27:28 +0500

    add

---
+++ main.js
@@ -0,0 +1,21 @@
+let btn = document.querySelector(".btn");
+let robot = document.querySelector(".robot");
+let play = document.getElementById("play");
+
+btn.addEventListener("click" , function(){
+    if(robot.classList.contains("hide")){
+        robot.classList.remove("hide");
+        alert("Show");
+        console.log("Show");
+    } else {
+        robot.classList.add("hide");
+        alert("Hide");
+        console.log("Hide");
+    }
+})
+
+function playMusic(){
+    let audio = new Audio("files/ElbekMusic.Sevgi.mp3");
+    audio.play()
+}
+play.addEventListener("click" , playMusic)
